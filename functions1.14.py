ebb5af30634dc0ce5e9c4d40604fcb913ea3eb65
DaniyarDaniyar <144439833+DaniyarDaniyar@users.noreply.github.com>
2023-10-14 19:02:04 +0600

    lab5
    lab5

---
+++ functions1.14.py
@@ -0,0 +1,17 @@
+from function1 import (
+    grams_in_ounces,
+    FinC,
+    rabbitandchikens,
+    primenums,
+    perm,
+    reverse,
+    has_33,
+    spy_game,
+    obyem_sphery,
+    uniq,
+    ispolindrome,
+    histogram,
+    gsn  
+)
+grams=int(input())
+grams_in_ounces(grams)
